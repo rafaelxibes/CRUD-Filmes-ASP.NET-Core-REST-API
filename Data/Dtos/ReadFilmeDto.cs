f6e1a964869acece9c4714e957446564aaae3389
RafaelXibes <rafaelxibes@hotmail.com>
2022-08-28 17:15:56 -0300

    Adicionar arquivos de projeto.

---
+++ Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class ReadFilmeDto
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo títilo é obrigatório!")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatório!")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero não deve passar de 30 caracteres!")]
+        public string Genero { get; set; }
+        [Range(1, 240, ErrorMessage = "A duração deverá ter mínima de 1 minuto e máximo de 240 minutos!")]
+        public int Duracao { get; set; }
+        public DateTime HoraDaConsulta { get; set; }
+    }
+}
---
+++ Data/Dtos/UpdateFilmeDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class UpdateFilmeDto
+    {
+        [Required(ErrorMessage = "O campo títilo é obrigatório!")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatório!")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero não deve passar de 30 caracteres!")]
+        public string Genero { get; set; }
+        [Range(1, 240, ErrorMessage = "A duração deverá ter mínima de 1 minuto e máximo de 240 minutos!")]
+        public int Duracao { get; set; }
+    }
+}
